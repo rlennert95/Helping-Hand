60d4787bfb73339ffd875f379b6f0af376989055
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2019-01-23 20:36:29 -0500

    updated homepage

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -107,7 +107,7 @@ class LoginForm extends Component {
                         </div>
                         <div className="card-footer">
                             <div className="d-flex justify-content-center links">
-                                Don't have an account?<a href="#">Sign Up</a>
+                                Don't have an account?<a href="signup">Sign Up</a>
                             </div>
                             <div className="d-flex justify-content-center">
                                 <a href="#">Forgot your password?</a>
